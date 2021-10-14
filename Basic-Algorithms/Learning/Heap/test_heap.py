8f2fa49f9d780f9d415b2114ff1e9da069d24ca2
Utkarsh Singh <us9044@gmail.com>
2021-10-14 09:49:00 +0530

    Basic Algorithms - 2

---
+++ Basic-Algorithms/Learning/Heap/test_heap.py
@@ -0,0 +1,23 @@
+from heap import *
+
+heap_size = 5
+heap = Heap(heap_size)
+
+elements = [1, 2, 3, 4, 1, 2]
+for element in elements:
+    heap.insert(element)
+print('Inserted elements: {}'.format(elements))
+
+print('size of heap: {}'.format(heap.size()))
+
+for _ in range(4):
+    print('Call remove: {}'.format(heap.remove()))
+
+print('Call get_minimum: {}'.format(heap.get_minimum()))
+
+for _ in range(2):
+    print('Call remove: {}'.format(heap.remove()))
+
+print('size of heap: {}'.format(heap.size()))
+print('Call remove: {}'.format(heap.remove()))
+print('Call is_empty: {}'.format(heap.is_empty()))
